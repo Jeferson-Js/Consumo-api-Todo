0d754d0eaa6cc5c6a88d06d9498fe40186b13d8a
Jeferson Santos Oliveira <Jeferson Santos Oliveira>
2022-11-13 21:02:53 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
-const list = document.querySelector("#fill_list");
-
 const fetchApi = async (item) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+    //const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+    const response = await fetch(`https://api-names.netlify.app/${item}`);
 
     const data = await response.json();
 
     return data;
-}
+};
 
 const renderApi = async (item) => {
-
-    const data = await fetchApi(item)
+    const data = await fetchApi(item);
 
     data.map((item) => {
-        const li = document.createElement('li');
-        li.setAttribute('id', item.id);
+        const list = document.querySelector("#fill_list");
+        const li = document.createElement("li");
+        li.setAttribute("id", item.id);
         li.innerHTML = item.title;
-        list.appendChild(li)
-    })
-}
+        list.appendChild(li);
+    });
+};
